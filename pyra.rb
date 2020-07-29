4443cd6f31fa9553914f76c2a1fe0b22df9ce3ac
Marcin Konowalczyk <marcin.konowalczyk@chem.ox.ac.uk>
2020-07-29 23:56:18 +0100

    cleanup the triangle parser code

--- pyra.rb
+++ pyra.rb
@@ -18,46 +18,28 @@ def triangle_from(lines, ptr_inds = nil)
     ptr_inds = ptr_inds || indices(lines.first, $TOP)
     row = ""
     ptr_inds.map { |pt|
-        x1 = x2 = pt    # left and right sides
+        x1 = x2 = pt # left and right sides
         y = 0
         data = []
         loop {
-            x1 -= 1
-            x2 += 1
-            y += 1
+            x1 -= 1; x2 += 1; y += 1 # Go to the next row, left and right 
             row = lines[y]
             raise "unexpected triangle end" if !row or x2 > row.size
-            # are we done?
-            if row[x1] != $L_SIDE
-                # look for end
-                if row[x2] == $R_SIDE # mismatched!
-                    raise "left side too short"
-                else
-                    # both sides are exhausted--look for a bottom
-                    # p (x1 + 1 .. x2 - 1).map { |e| row[e] }
-                    # p [x1, x2, pt]
-                    if (x1 + 1 .. x2 - 1).all? { |x| row[x] == $BOTTOM }
-                        break
-                    else
-                        raise "malformed bottom"
-                    end
-                end
-            elsif row[x2] != $R_SIDE
-                # look for end
-                if row[x1] == $L_SIDE # mismatched!
-                    raise "right side too short"
-                else
-                    # both sides are exhausted--look for a bottom
-                    if (x1 + 1 .. x2 - 1).all? { |x| row[x] == $BOTTOM }
-                        break
-                    else
-                        raise "malformed bottom"
-                    end
-                end
-            # elsif x1 == 0   # we must have found our bottom...
+            
+            # Check for mismatched sides
+            r_side = row[x2] == $R_SIDE
+            l_side = row[x1] == $L_SIDE
+            raise "right side too short" if l_side and not r_side
+            raise "left side too short" if r_side and not l_side
+            
+            # Are we done?
+            if not l_side and not r_side
+                correct_bottom = (x1 + 1 .. x2 - 1).all? { |x| row[x] == $BOTTOM }
+                raise "malformed bottom" if not correct_bottom
+                break
             end
-            #todo: do for other side
-            # we aren't done.
+
+            # We aren't done
             data.push row[x1 + 1 .. x2 - 1]
         }
         op = data.join("").gsub(/\s+/, "")
